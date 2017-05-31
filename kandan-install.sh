8226c9d271334881c4acd9e44dd65a39f27517e4
xeredin <bilisim@yandex.com>
2017-05-31 12:46:41 +0200

    Update kandan-install.sh

--- kandan-install.sh
+++ kandan-install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # Hayrettin Kilic CentOS 6x Kandan Chat Build Script
 # hayrettin@kilic.email
+
+#Requirement installations
+yum -y groupinstall "Development Tools"
+
+yum -y install gcc gcc-c++
+
+yum -y install git
+
+yum -y install libxslt-devel libxml2-devel postgresql-devel
+
+yum -y install mysql-server mysql-devel
