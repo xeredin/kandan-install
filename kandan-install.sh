806d35a37f1633933a58a2eace19366c50a64159
Hayrettin Kilic <bilisim@yandex.com>
2017-05-31 15:30:51 +0200

    Update kandan-install.sh

--- kandan-install.sh
+++ kandan-install.sh
@@ -12,6 +12,10 @@ echo "1. CentOS 6x"
 echo -n "Please choose a OS [1]? "
 read choice
 if [ $choice = 1 ] ; then
+
+	echo "Please enter a user password for mysql database;"
+	read mysqlpass
+	wait
 	echo "Requirements are being set up."
 	wait
 	
@@ -26,6 +30,13 @@ if [ $choice = 1 ] ; then
     wait
     yum -y install mysql-server mysql-devel
     wait
+    service mysqld start
+    chkconfig mysqld on
+    wait
+    mysql -u root -p -e 'create database db_kandan default character set utf8;'
+    mysql -u root -p -e 'grant all on db_kandan.* to user_kandan@localhost identified by '$mysqlpass';'
+    mysql -u root -p -e 'flush privileges;'
+    wait
     yum -y install epel-release
     wait
     yum -y install libyaml libyaml-devel --enablerepo=epel
@@ -37,7 +48,9 @@ if [ $choice = 1 ] ; then
     iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
     iptables -A INPUT -p tcp -m tcp --dport 3000 -j ACCEPT
     iptables -t nat -A PREROUTING -p tcp --dport 3000 -j REDIRECT --to-port 80
-    
+    clear
+    echo "Ruby installation and gem update is starting."
+    wait
     # Ruby download and install
     wget https://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz
     wait
@@ -46,6 +59,11 @@ if [ $choice = 1 ] ; then
     ./configure --disable-install-doc
     wait
     make;make install
+    wait
+    gem update --system --no-rdoc --no-ri
+    gem install bundler --no-rdoc --no-ri
+    clear
+    
     
     
     
